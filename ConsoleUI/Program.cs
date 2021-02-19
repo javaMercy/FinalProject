2ee04aff1934dfdc4fc6d916c4293e74539fc71c
JavaMercy <kursunemre1221@gmail.com>
2021-02-19 23:12:49 +0300

    February 6

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,13 +9,33 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            //categoryTest();
+
+
+        }
+
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(20,22))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine( "{0} - {1} " ,product.ProductName,product.UnitPrice);
+                Console.WriteLine("{0} - {1} ", product.ProductName, product.CategoryName);
             }
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+            Console.WriteLine(categoryManager.GetById(2).CategoryName);
         }
     }
 }
- 
